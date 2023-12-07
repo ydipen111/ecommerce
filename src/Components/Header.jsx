a99e625bb5b6b69bee53115c8f1669f84f2a919c
dipen <mgrd08716@gmail.com>
2023-12-07 23:32:59 +0545

    Components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Header() {
+  return <> 
+  <div className='bg-[#22d3ee] p-4'>
+    <div className='max-w-[1280px] items-center flex justify-between border-2 mx-auto'>
+    
+    <div className='text-3xl'>
+        WsCubeTech
+    </div>
+    <ul className='flex text-white gap-5'>
+    <li>Home</li>
+    <li>Services</li>
+    <li>Company</li>
+    <li>Resources</li>
+    <li>About </li>    
+    </ul>   
+    </div>   
+
+    </div>
+    </>
+}

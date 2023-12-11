921b4538bac9c5d0013f7f3c26a946d57fdba1e6
dipen <mgrd08716@gmail.com>
2023-12-11 15:21:57 +0545

    hidden and flex

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,37 @@
 import React from 'react'
+import { IoMenuOutline, IoCloseOutline  } from "react-icons/io5";
 
 export default function Header() {
   return <> 
-  <div className='bg-[#22d3ee] p-4'>
-    <div className='max-w-[1280px] items-center flex justify-between border-2 mx-auto'>
+  {/* <div className='bg-[#22d3ee] '> */}
+    <div className='max-w-100% bg-[#22d3ee] py-15px items-center flex justify-between border-2 mx-auto'>
     
     <div className='text-3xl'>
         WsCubeTech
     </div>
-    <ul className='flex text-white gap-5'>
+    <IoMenuOutline className='text-white text-2xl font-bold md:hidden block'/>
+     
+    <IoCloseOutline className='text-white text-2xl  font-bold md:hidden block'/>
+   
+    <ul className=' text-white md:hidden gap-10'>
     <li>Home</li>
     <li>Services</li>
     <li>Company</li>
     <li>Resources</li>
     <li>About </li>    
     </ul>   
+    {/* //responsive */}
+    <ul className=' text-white md:hidden fixed bg-black left-0 top-[122px]'>
+    <li>Home</li>
+    <li>Services</li>
+    <li>Company</li>
+    <li>Resources</li>
+    <li>About </li>    
+    </ul>   
+    
+
     </div>   
 
-    </div>
+    {/* </div> */}
     </>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
-    extend: {},
+    extend:{
+      colors: {
+        'primary':"#FB2E86",
+        'sec':"#7E33E0",
+        'secDark':"#151875",
+        'secl':'#EEEFFB',
+        'orchid':"#C71585	",
+        'third':"#8B4513",
+        'fourt':"#BC8F8F",
+
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... 
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+        'xxl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+      backgroundImage:{
+        'ecom':"url('./src/assets/ecom.jpg')",
+        'ecom2':"url('./src/assets/ecom2.jpg')",
+        'flat':"url('./src/assets/flat.jpg')"
+      }
+    }
   },
+
   plugins: [],
 }
 

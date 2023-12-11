3b0e546cdc4f5868a421770bd74fbb5519ecbdb7
dipen <mgrd08716@gmail.com>
2023-12-11 23:58:49 +0545

    learn.txt

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoMenuOutline, IoCloseOutline  } from "react-icons/io5";
 
 export default function Header() {
+  const [toggle, setToggle]=useState(false)
   return <> 
   {/* <div className='bg-[#22d3ee] '> */}
     <div className='max-w-100% bg-[#22d3ee] py-15px items-center flex justify-between border-2 mx-auto'>
@@ -9,11 +10,12 @@ export default function Header() {
     <div className='text-3xl'>
         WsCubeTech
     </div>
-    <IoMenuOutline className='text-white text-2xl font-bold md:hidden block'/>
-     
-    <IoCloseOutline className='text-white text-2xl  font-bold md:hidden block'/>
-   
-    <ul className=' text-white md:hidden gap-10'>
+    { toggle ?
+    <IoMenuOutline  onClick={()=>setToggle(!toggle)} className='text-white text-2xl font-bold md:hidden block'/>
+     :
+    <IoCloseOutline onClick={()=>setToggle(!toggle)}  className='text-white text-2xl  font-bold md:hidden block'/>
+}
+    <ul className=' text-white md:flex hidden gap-10'>
     <li>Home</li>
     <li>Services</li>
     <li>Company</li>
@@ -21,12 +23,12 @@ export default function Header() {
     <li>About </li>    
     </ul>   
     {/* //responsive */}
-    <ul className=' text-white md:hidden fixed bg-black left-0 top-[122px]'>
-    <li>Home</li>
-    <li>Services</li>
-    <li>Company</li>
-    <li>Resources</li>
-    <li>About </li>    
+    <ul className=' text-white w-full screen h-screen md:hidden fixed bg-black left-[-100%] top-[39px]'>
+    <li className='p-5 text-left'>Home</li>
+    <li className='p-5 text-left'>Services</li>
+    <li className='p-5 text-left'>Company</li>
+    <li className='p-5 text-left'>Resources</li>
+    <li className='p-5 text-left'>About </li>    
     </ul>   
     
 
